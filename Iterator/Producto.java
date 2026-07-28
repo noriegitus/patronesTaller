f79034f1451647b5440fb9763ce38e0b99a15489
RomeroAbraham <abrarome@espol.edu.ec>
2026-07-27 19:11:35 -0500

    Add Producto class with attributes and methods

---
+++ Iterator/Producto.java
@@ -0,0 +1,21 @@
+public class Producto {
+    private String nombre;
+    private double precio;
+    private boolean disponible;
+    private boolean enGarantia;
+    private boolean conFalla;
+
+    public Producto(String nombre, double precio, boolean disponible, boolean enGarantia, boolean conFalla) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.disponible = disponible;
+        this.enGarantia = enGarantia;
+        this.conFalla = conFalla;
+    }
+
+    public String getNombre() { return nombre; }
+    public double getPrecio() { return precio; }
+    public boolean isDisponible() { return disponible; }
+    public boolean isEnGarantia() { return enGarantia; }
+    public boolean isConFalla() { return conFalla; }
+}
