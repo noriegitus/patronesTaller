da14bd7741b149b1c0737bf1d792756327f317b7
RomeroAbraham <abrarome@espol.edu.ec>
2026-07-28 10:04:35 -0500

    Change interface implementation from RutaDistribucion to EstrategiaTransporte

--- strategy/TransporteAutomotriz.java
+++ strategy/TransporteAutomotriz.java
@@ -1,7 +1,7 @@
 package strategy;
 import java.util.List;
 
-public class TransporteAutomotriz implements RutaDistribucion {
+public class TransporteAutomotriz implements EstrategiaTransporte {
     @Override
     public void realizarEnvio(List<Producto> productos, String destino) {
         System.out.println("Enviando " + productos.size() + " productos vía automotriz a: " + destino);
