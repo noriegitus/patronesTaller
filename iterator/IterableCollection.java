3dde99c9ce19370544d20036b8f06b075e8373ea
RomeroAbraham <abrarome@espol.edu.ec>
2026-07-28 12:04:19 -0500

    Delete iterator directory

--- iterator/IterableCollection.java
+++
@@ -1,6 +0,0 @@
-package iterator;
-
-public interface IterableCollection {
-    Iterator createDisponibleIterator();
-    Iterator createGarantiaFallasIterator();
-}
--- iterator/Iterator.java
+++
@@ -1,6 +0,0 @@
-package iterator;
-
-public interface Iterator {
-    boolean hasMore();
-    Producto getNext();
-}
