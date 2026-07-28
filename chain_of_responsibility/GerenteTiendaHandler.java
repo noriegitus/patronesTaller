60e9d2a3f3aee3623b3c9c483bac9224d3b810ee
RomeroAbraham <abrarome@espol.edu.ec>
2026-07-28 11:12:11 -0500

    Update GerenteTiendaHandler.java

--- chain_of_responsibility/GerenteTiendaHandler.java
+++ chain_of_responsibility/GerenteTiendaHandler.java
@@ -3,11 +3,11 @@ package chain_of_responsibility;
 public class GerenteTiendaHandler extends BaseHandler {
     @Override
     public boolean handle(SolicitudCambio solicitud) {
-        if (solicitud.getProducto().getPrecio() > 1000) {
+      if (solicitud.getMonto() > 1000) {
             System.out.println("Gerente de Tienda: Aprobación requerida por superar los $1000 USD. APROBADO.");
         } else {
             System.out.println("Gerente de Tienda: No requiere aprobación especial (menor a $1000 USD).");
         }
-        return super.handle(solicitud);
+        super.handle(solicitud);
     }
 }
