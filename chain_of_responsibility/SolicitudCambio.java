a2577179823d9b90f9fb5f8a406753a17a40d2c6
RomeroAbraham <abrarome@espol.edu.ec>
2026-07-28 11:14:59 -0500

    Update SolicitudCambio.java

--- chain_of_responsibility/SolicitudCambio.java
+++ chain_of_responsibility/SolicitudCambio.java
@@ -1,20 +1,19 @@
 package chain_of_responsibility;
 
 public class SolicitudCambio {
-    private Producto producto;
-    private boolean dentroDeGarantia;
-    private boolean falloCubierto;
-    private boolean existeEnBodega;
+    private String motivo;
+    private double monto;
 
-    public SolicitudCambio(Producto producto, boolean dentroDeGarantia, boolean falloCubierto, boolean existeEnBodega) {
-        this.producto = producto;
-        this.dentroDeGarantia = dentroDeGarantia;
-        this.falloCubierto = falloCubierto;
-        this.existeEnBodega = existeEnBodega;
+    public SolicitudCambio(String motivo, double monto) {
+        this.motivo = motivo;
+        this.monto = monto;
     }
 
-    public Producto getProducto() { return producto; }
-    public boolean isDentroDeGarantia() { return dentroDeGarantia; }
-    public boolean isFalloCubierto() { return falloCubierto; }
-    public boolean isExisteEnBodega() { return existeEnBodega; }
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
 }
