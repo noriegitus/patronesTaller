e22368a35dfb168dd74088bb540cb05f59b3efb0
RomeroAbraham <abrarome@espol.edu.ec>
2026-07-28 11:05:50 -0500

    Update TransporteFerreo.java

--- strategy/TransporteFerreo.java
+++ strategy/TransporteFerreo.java
@@ -1,7 +1,7 @@
 package strategy;
 import java.util.List;
 
-public class TransporteAutomotriz implements RutaDistribucion {
+public class TransporteFerreo implements EstrategiaTransporte {
     @Override
     public void realizarEnvio(List<Producto> productos, String destino) {
         System.out.println("Enviando " + productos.size() + " productos vía férrea a: " + destino);
