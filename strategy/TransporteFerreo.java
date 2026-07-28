30138971ea55396121ac94e13b4388e916def09f
RomeroAbraham <abrarome@espol.edu.ec>
2026-07-28 10:05:46 -0500

    Add TransporteAutomotriz class for rail distribution

---
+++ strategy/TransporteFerreo.java
@@ -0,0 +1,9 @@
+package strategy;
+import java.util.List;
+
+public class TransporteAutomotriz implements RutaDistribucion {
+    @Override
+    public void realizarEnvio(List<Producto> productos, String destino) {
+        System.out.println("Enviando " + productos.size() + " productos vía férrea a: " + destino);
+    }
+}
