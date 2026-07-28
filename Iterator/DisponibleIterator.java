9844fa141247e1c55eaf92e53ef51621209d054e
RomeroAbraham <abrarome@espol.edu.ec>
2026-07-27 19:12:22 -0500

    Implement DisponibleIterator for available products

---
+++ Iterator/DisponibleIterator.java
@@ -0,0 +1,27 @@
+public class DisponibleIterator implements Iterator<Producto> {
+    private Inventario inventario;
+    private int posicion = 0;
+
+    public DisponibleIterator(Inventario inventario) {
+        this.inventario = inventario;
+    }
+
+    @Override
+    public boolean hasMore() {
+        while (posicion < inventario.getProductos().size()) {
+            if (inventario.getProductos().get(posicion).isDisponible()) {
+                return true;
+            }
+            posicion++;
+        }
+        return false;
+    }
+
+    @Override
+    public Producto getNext() {
+        if (hasMore()) {
+            return inventario.getProductos().get(posicion++);
+        }
+        return null;
+    }
+}
