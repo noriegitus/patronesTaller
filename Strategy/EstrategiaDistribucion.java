0d00550f281afe7e8ed6f2c1e20514ce04f3da35
RomeroAbraham <abrarome@espol.edu.ec>
2026-07-27 19:15:11 -0500

    Create EstrategiaDistribucion.java

---
+++ Strategy/EstrategiaDistribucion.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class EstrategiaDistribucion {
+    private RutaDistribucion strategy;
+
+    public void setStrategy(RutaDistribucion strategy) {
+        this.strategy = strategy;
+    }
+
+    public void ejecutarEnvio(List<Producto> productos, String destino) {
+        if (strategy == null) {
+            throw new IllegalStateException("Estrategia de distribución no configurada.");
+        }
+        strategy.realizarEnvio(productos, destino);
+    }
+}
