747e5a3e636c4e7fb7c145d92b699b9ba373157f
RomeroAbraham <abrarome@espol.edu.ec>
2026-07-28 11:17:31 -0500

    Update DptoTecnicoHandler.java

--- chain_of_responsibility/DptoTecnicoHandler.java
+++ chain_of_responsibility/DptoTecnicoHandler.java
@@ -1,13 +1,11 @@
 package chain_of_responsibility;
 
 public class DptoTecnicoHandler extends BaseHandler {
+    
     @Override
-    public boolean handle(SolicitudCambio solicitud) {
-        if (!solicitud.isFalloCubierto()) {
-            System.out.println("Dpto. Técnico: El tipo de fallo no está cubierto por la garantía.");
-            return false;
-        }
-        System.out.println("Dpto. Técnico: Fallo validado correctamente.");
-        return super.handle(solicitud);
+    public void handle(SolicitudCambio solicitud) {
+        System.out.println("Dpto. Técnico: Constatando que el fallo ('" + solicitud.getMotivo() + "') esté cubierto... Validado.");
+        
+        super.handle(solicitud);
     }
 }
