61e3a344fde4ba1817307eca4ba7bb085221573f
RomeroAbraham <abrarome@espol.edu.ec>
2026-07-27 19:12:00 -0500

    Implement Inventario class with product management

---
+++ Iterator/Inventario.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventario implements IterableCollection {
+    private List<Producto> productos = new ArrayList<>();
+
+    public void agregarProducto(Producto p) {
+        productos.add(p);
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    @Override
+    public Iterator<Producto> createDisponibleIterator() {
+        return new DisponibleIterator(this);
+    }
+
+    @Override
+    public Iterator<Producto> createGarantiaFallasIterator() {
+        return new GarantiaFallasIterator(this);
+    }
+}
